e59bf93824d401d6d8ba8efe25df4986d1cac691
dawit kindea <davekindea1993@gmail.com>
2024-10-10 00:23:44 +0300

    task1

--- noatbooks/task3/sales/serializers.py
+++
@@ -1,4 +0,0 @@
-from rest_framework import serializers
-
-class SalesDataSerializer(serializers.Serializer):
-    sales = serializers.ListField(child=serializers.FloatField())
--- noatbooks/task3/sales/urls.py
+++
@@ -1,8 +0,0 @@
-
-# sales/urls.py
-from django.urls import path
-from .views import SalesPredictionView
-
-urlpatterns = [
-    path('predict/', SalesPredictionView.as_view(), name='predict-sales'),  # Correct endpoint
-]
